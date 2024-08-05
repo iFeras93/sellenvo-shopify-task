9beae6841184a351443f282704e8ca8e456ff9ff
Feras Mohamed <ferasmo7mad@gmail.com>
2024-08-05 03:25:04 +0300

    Define routes and controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Contracts\ProductApiInterface;
+use App\Imports\ProductsImport;
+use App\Jobs\PrepareDataBeforePushToShopify;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Exception\RequestException;
+use Illuminate\Http\File;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ProductController extends Controller
 {
@@ -19,8 +24,56 @@ class ProductController extends Controller
 
     public function getProductsList()
     {
-        return $this->productApi->getProducts();
+        $cleanedProducts = $this->modifyProductTitles($this->productApi->getProducts()['products']);
+        return json_encode($cleanedProducts, JSON_PRETTY_PRINT);
     }
 
+    public function handleStoringProduct()
+    {
+        $excelFile = Storage::path('public/products_export.csv');
+        PrepareDataBeforePushToShopify::dispatch($excelFile);
+        return 'Imported in progress... ' . time();
+    }
+
+    private function cleanObject(&$object, &$modifyTitle = false)
+    {
+        if (is_array($object)) {
+            foreach ($object as $key => &$value) {
+                if (is_array($value) || is_object($value)) {
+                    $this->cleanObject($value, $modifyTitle);
+                } else {
+                    if ($value === 'N/A' || $value === '-' || $value === '' || $value === null) {
+                        unset($object[$key]);
+                        $modifyTitle = true;
+                    }
+                }
+            }
+        } elseif (is_object($object)) {
+            foreach ($object as $key => &$value) {
+                if (is_array($value) || is_object($value)) {
+                    $this->cleanObject($value, $modifyTitle);
+                } else {
+                    if ($value === 'N/A' || $value === '-' || $value === '' || $value === null) {
+                        unset($object->$key);
+                        $modifyTitle = true;
+                    }
+                }
+            }
+        }
+
+        return $object;
+    }
+
+    private function modifyProductTitles($products)
+    {
+        foreach ($products as &$product) {
+            $modifyTitle = false;
+            $this->cleanObject($product, $modifyTitle);
+            if ($modifyTitle) {
+                $product['title'] .= ' nullable';
+            }
+        }
+        return $products;
+    }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/products-list', [ProductController::class, 'getProductsList']);
+Route::get('/products-store', [ProductController::class, 'handleStoringProduct']);
